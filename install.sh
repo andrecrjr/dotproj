73caff72f3c9d98fef1f7b2f6cbe39f2258fcaab
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-06-07 12:23:43 -0300

    update install script

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@ echo "Installing DotProj..."
 curl -fsSL https://raw.githubusercontent.com/andrecrjr/dotproj/master/dotproj -o dotproj
 chmod +x dotproj
 ./dotproj setup
-rm dotproj
 echo "Installation complete! Please restart your terminal or run: source ~/.bashrc"
